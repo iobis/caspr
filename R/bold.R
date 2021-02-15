a54b1ba234cf30d84fb2f28540934a164aa96b22
Pieter Provoost <pieterprovoost@gmail.com>
2021-02-15 20:43:44 +0100

    bold statistics

---
+++ R/bold.R
@@ -0,0 +1,19 @@
+#' Get statistics on marker sequences in BOLD by taxon
+#'
+#' @param taxon taxon name
+#' @return a tibble
+#' @export
+bold_stats <- function(taxon) {
+  res <- bold::bold_seqspec(taxon)
+  if (!is.data.frame(res)) {
+    return(tibble())
+  }
+  df <- res %>%
+    bind_rows() %>%
+    select(sampleid, institution_storing, sequenceID, markercode, genbank_accession, nucleotides, seq_primers)
+  marker_stats <- df %>%
+    group_by(markercode) %>%
+    summarize(sequences = n(), .groups = "drop") %>%
+    filter(!is.na(markercode) & markercode != "")
+  return(as_tibble(marker_stats))
+}
--- R/checklists.R
+++ R/checklists.R
@@ -1,19 +1,30 @@
 #' Fetch expert priority checklists from the PacMAN GitHub repository.
 #'
 #' @param summarize group by taxon and concatenate references and remarks.
+#' @param enhance add complete taxonomy from OBIS.
 #' @return The expert priority checklist.
 #' @export
-expert_checklists <- function(summarize = TRUE) {
+expert_checklists <- function(summarize = TRUE, enhance = FALSE) {
   df <- read.csv("https://raw.githubusercontent.com/iobis/pacman/main/SpeciesLists/SpeciesList.tsv?token=AADXUOMJQ24AIINJURWKUYDAFLISW", sep = "\t") %>%
-    select(taxonID = AphiaID_accepted, scientificName = scientificName_accepted, references, remarks = taxonRemarks) %>%
-    mutate(taxonID = suppressWarnings(as.numeric(taxonID)))
+    select(taxonID = AphiaID_accepted, scientificName = scientificName_accepted, references, remarks = taxonRemarks, taxonRank) %>%
+    mutate(taxonID = suppressWarnings(as.numeric(taxonID))) %>%
+    filter(!is.na(taxonID))
   if (summarize == TRUE) {
-    df %>%
-      group_by(taxonID, scientificName) %>%
-      summarize(references = paste0(stri_remove_empty_na(references), collapse = "; "), remarks = paste0(stri_remove_empty_na(remarks), collapse = "; "))
-  } else {
-    df
+    df <- df %>%
+      group_by(taxonID, scientificName, taxonRank) %>%
+      summarize(references = paste0(stri_remove_empty_na(references), collapse = "; "), remarks = paste0(stri_remove_empty_na(remarks), collapse = "; ")) %>%
+      ungroup()
   }
+  if (enhance == TRUE) {
+    taxa <- taxon(df$taxonID)
+    df <- df %>%
+      select(-c(taxonRank)) %>%
+      left_join(taxa, by = "taxonID") %>%
+      mutate(scientificName.y = ifelse(is.na(scientificName.y), scientificName.x, scientificName.y)) %>%
+      select(-scientificName.x) %>%
+      rename(scientificName = scientificName.y)
+  }
+  df
 }
 
 #' Fetch a checklist from OBIS.
