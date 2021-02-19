7be684795ee7387db6dec5256a3df3b1ca67fb74
Pieter Provoost <pieterprovoost@gmail.com>
2021-02-19 19:51:02 +0100

    fixes to expert checklist

--- R/checklists.R
+++ R/checklists.R
@@ -5,28 +5,22 @@ wkt_south_pacific <- "POLYGON ((-218 -62, -218 14, -86 14, -67 -23, -74 -62, -21
 #' Fetch expert priority checklists from the PacMAN GitHub repository.
 #'
 #' @param summarize group by taxon and concatenate references and remarks.
-#' @param enhance add complete taxonomy from OBIS.
 #' @return The expert priority checklist.
 #' @export
-expert_checklists <- function(summarize = TRUE, enhance = FALSE) {
-  df <- read.csv("https://raw.githubusercontent.com/iobis/pacman/main/SpeciesLists/SpeciesList.tsv?token=AADXUOMJQ24AIINJURWKUYDAFLISW", sep = "\t") %>%
+expert_checklists <- function(summarize = TRUE) {
+  df <- googlesheets4::read_sheet("https://docs.google.com/spreadsheets/d/1s45jM7oR4lbURG5JN1r6XamoWjGMZt7hnexAgc8yTYY/edit?usp=sharing") %>%
     select(taxonID = AphiaID_accepted, scientificName = scientificName_accepted, references, remarks = taxonRemarks, taxonRank) %>%
     mutate(taxonID = suppressWarnings(as.numeric(taxonID))) %>%
     filter(!is.na(taxonID))
+  records <- bind_rows(worrms::wm_record_(df$taxonID)) %>%
+    select(AphiaID, phylum, class, family)
+  df <- df %>%
+    left_join(records, by = c("taxonID" = "AphiaID"))
   if (summarize == TRUE) {
     df <- df %>%
-      group_by(taxonID, scientificName, taxonRank) %>%
-      summarize(references = paste0(stri_remove_empty_na(references), collapse = "; "), remarks = paste0(stri_remove_empty_na(remarks), collapse = "; ")) %>%
+      group_by(taxonID, scientificName, taxonRank, phylum, class, family) %>%
+      summarize(references = paste0(stri_remove_empty_na(unique(references)), collapse = "; "), remarks = paste0(stri_remove_empty_na(unique(remarks)), collapse = "; ")) %>%
       ungroup()
-  }
-  if (enhance == TRUE) {
-    taxa <- taxon(df$taxonID)
-    df <- df %>%
-      select(-c(taxonRank)) %>%
-      left_join(taxa, by = "taxonID") %>%
-      mutate(scientificName.y = ifelse(is.na(scientificName.y), scientificName.x, scientificName.y)) %>%
-      select(-scientificName.x) %>%
-      rename(scientificName = scientificName.y)
   }
   df
 }
