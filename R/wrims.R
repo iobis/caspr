db4113fbcc9f25c5675538522591446b34d9805f
Pieter Provoost <pieterprovoost@gmail.com>
2022-05-27 14:48:25 +0200

    added distribution plotting

--- R/wrims.R
+++ R/wrims.R
@@ -1,4 +1,4 @@
-#' Get the complete WRiMS checklist
+#' Get the complete WRiMS checklist. This may not be complete due to missing macroalgae.
 #'
 #' @export
 wrims_checklist <- function() {
@@ -38,4 +38,59 @@ wrims_checklist <- function() {
       }
       counter <- counter + 1
     }
+}
+
+mr_geometries <- function(mrgid) {
+  url <- glue::glue("https://marineregions.org/rest/getGazetteerGeometries.jsonld/{mrgid}/")
+  message(url)
+  geom <- jsonlite::fromJSON(url)$"mr:hasGeometry"$"gsp:asWKT"
+  if (is.null(geom)) {
+    return(st_sfc(st_polygon()))
+  } else {
+    geom_fixed <- stringr::str_replace(geom, "<.*>\\s+", "")
+    geom_union <- st_union(st_as_sfc(geom_fixed))
+    return(geom_union)
+  }
+}
+
+wrims_distribution <- function(aphiaid, simplify = 0.5) {
+  dist <- worrms::wm_distribution(aphiaid) %>%
+    mutate(mrgid = stringr::str_replace_all(locationID, "http://marineregions.org/mrgid/", ""))
+  mrgids <- unique(dist$mrgid)
+  geom <- sapply(mrgids, mr_geometries)
+  spatial <- tibble(geom = geom, mrgid = names(geom)) %>%
+    as.data.frame()
+  dist <- dist %>%
+    left_join(spatial, by = "mrgid") %>%
+    st_as_sf(crs = 4326)
+  if (!is.na(simplify) && !is.null(simplify)) {
+    sf_use_s2(FALSE)
+    dist <- dist %>%
+      st_simplify(dTolerance = simplify)
+  }
+  return(dist)
+}
+
+world <- rnaturalearth::ne_countries(scale = "large", returnclass = "sf")
+
+plot_wrims_dist <- function(dist) {
+  ggplot() +
+    geom_sf(data = world, fill = NA, size = 0.1, color = "#000000") +
+    geom_sf(data = dist %>% filter(!is.na(establishmentMeans)), aes(fill = establishmentMeans, color = establishmentMeans), alpha = 0.5, size = 0.1) +
+    ggpattern::geom_sf_pattern(data = dist %>% filter(is.na(establishmentMeans)), alpha = 1, size = 0.2, fill = NA, color = "#02a4cc", pattern_size = 0.1, pattern_color = "#02a4cc", pattern_spacing = 0.02, pattern_density = 0.5, pattern_fill = NA) +
+    scale_fill_manual(values = c("Alien" = "#9c491f", "Native" = "#649c1f"), na.value = NA) +
+    scale_color_manual(values = c("Alien" = "#9c491f", "Native" = "#649c1f"), na.value = NA) +
+    theme_void() +
+    theme(legend.position = "bottom")
+}
+
+plot_obis_dist <- function(aphiaid) {
+  url <- glue::glue("https://api.obis.org/occurrence/grid/3?taxonid={aphiaid}")
+  json <- httr::GET(URLencode(url)) %>% httr::content(as = "text")
+  dist <- geojsonsf::geojson_sf(json)
+  ggplot() +
+    geom_sf(data = world, fill = NA, size = 0.1, color = "#000000") +
+    geom_sf(data = dist, alpha = 0.7, size = 0.5, color = "#fc5603", fill = "#fc5603") +
+    theme_void() +
+    theme(legend.position = "bottom")
 }
